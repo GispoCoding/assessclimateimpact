0a03cd9ee89f04bf13397ccbe59735c32dbac638
Mikael Vaaltola <mvaaltola@gmail.com>
2019-07-03 14:49:32 +0300

    Terminate task on error instead raising exception

--- ykr_tool_tasks.py
+++ ykr_tool_tasks.py
@@ -1,44 +1,42 @@
-import psycopg2
-from qgis.core import (QgsTask)
+from qgis.core import (QgsTask, QgsMessageLog, Qgis)
 from .createdbconnection import createDbConnection
 
 class QueryTask(QgsTask):
-    def __init__(self, description, connParams, queries):
-        super().__init__(description, QgsTask.CanCancel)
+    def __init__(self, connParams, queries):
+        super().__init__('Suoritetaan laskentaa', QgsTask.CanCancel)
         self.exception = None
         self.conn = None
         self.queries = queries
         try:
             self.conn = createDbConnection(connParams)
             self.cur = self.conn.cursor()
-        except psycopg2.Error as e:
+        except Exception as e:
             self.exception = e
 
     def run(self):
         if self.exception:
             return False
 
-        try:
-            i = 0
-            for query in self.queries:
-                self.setProgress(i / len(self.queries) * 100)
+        i = 0
+        for query in self.queries:
+            self.setProgress(i / len(self.queries) * 100)
+            i += 1
+            if self.isCanceled():
+                self.exception = 'Laskenta keskeytetty'
+                return False
+            try:
                 self.cur.execute(query)
-                self.conn.commit()
-                i += 1
-
-            self.setProgress(99)
-            return True
-
-        except psycopg2.Error as e:
-            self.exception = e
-            self.conn.rollback()
-            return False
+            except Exception as e:
+                self.exception = e
+                self.conn.rollback()
+                self.conn.close()
+                return False
+        self.conn.commit()
+        self.conn.close()
+        return True
 
     def finished(self, result):
-        if result:
-            self.conn.commit()
-            self.conn.close()
-        else:
-            if self.conn:
-                self.conn.close()
-            raise self.exception
+        if not result:
+            QgsMessageLog.logMessage('Laskentavirhe: ' + str(self.exception), 'YKRTool', Qgis.Critical)
+            # raise self.exception
+            self.cancel()
